7d73d9c655aa1bf39d4b111df8c78bdb5c8d96e6
rakkom <127444615+rakkom@users.noreply.github.com>
2023-04-21 20:29:13 +0200

    Update buy_and_sell.py

--- buy_and_sell.py
+++ buy_and_sell.py
@@ -1,53 +1,63 @@
 import json
 import argparse
+import os
 import bybit
 import pybit
 import config
 from pybit.unified_trading import HTTP
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--side", help="Trade side (Buy or Sell)", required=True)
+parser.add_argument("--side", help="Trade side (Buy or Sell)")
+parser.add_argument("--ticker", help="Ticker symbol")
 args = parser.parse_args()
 side = args.side
+ticker = args.ticker
+
+if args.ticker:
+    ticker_filename = args.ticker.lower() + ".json"
+    with open(os.path.join("ticker_file", ticker_filename), "r") as f:
+        settings = json.load(f)
+else:
+    settings = {"qty": "1"}
 
 session = HTTP(
     testnet=False,
-    api_key=config.your_api_key,
-    api_secret=config.your_api_secret,
+    api_key=config.api_key,
+    api_secret=config.api_secret,
 )
 
 if side.lower() == "buy":
     print(session.place_order(
         category="linear",
-        symbol="DOGEUSDT",
+        symbol=ticker,
         side="Buy",
         orderType="Market",
-        qty="500",
+        qty=settings["qty"],
         reduce_only=True
     ))
     print(session.place_order(
         category="linear",
-        symbol="DOGEUSDT",
+        symbol=ticker,
         side="Buy",
         orderType="Market",
-        qty="500",
+        qty=settings["qty"],
         reduce_only=False
     ))
 
 if side.lower() == "sell":
     print(session.place_order(
         category="linear",
-        symbol="DOGEUSDT",
+        symbol=ticker,
         side="Sell",
         orderType="Market",
-        qty="500",
+        qty=settings["qty"],
         reduce_only=True
     ))
     print(session.place_order(
         category="linear",
-        symbol="DOGEUSDT",
+        symbol=ticker,
         side="Sell",
         orderType="Market",
-        qty="500",
+        qty=settings["qty"],
         reduce_only=False
     ))
