f921e04441c53544cdbc3725bd156aba23e2cfae
rakkom <127444615+rakkom@users.noreply.github.com>
2023-04-24 01:06:05 +0200

    Update buy_and_sell_better.py

--- buy_and_sell_better.py
+++ buy_and_sell_better.py
@@ -1,26 +1,15 @@
-import os
-import json
-import argparse
-import pybit
-import config
+import os, json, argparse, pybit, config
 from pybit.unified_trading import HTTP
 
 def place_order(session, ticker, side, qty, reduce_only=False):
     try:
-        response = session.place_order(
-            category="linear",
-            symbol=ticker,
-            side=side,
-            orderType="Market",
-            qty=qty,
-            reduce_only=reduce_only
-        )
-        print(response)
+        r = session.place_order(category="linear", symbol=ticker, side=side, orderType="Market", qty=qty, reduce_only=reduce_only)
+        print(r)
     except Exception as e:
         print(f"Error placing {side} order: {e}")
 
 def place_order_with_reduce(session, ticker, side, qty):
-    place_order(session, ticker, side, qty, reduce_only=True)
+    place_order(session, ticker, side, qty, True)
     place_order(session, ticker, side, qty)
 
 parser = argparse.ArgumentParser()
@@ -28,28 +17,18 @@ parser.add_argument("--ticker", help="Ticker symbol")
 parser.add_argument("--side", help="Trade side (Buy or Sell)")
 parser.add_argument("--qty", help="Quantity")
 args = parser.parse_args()
-ticker = args.ticker.upper()
-side = args.side.capitalize()
-qty = args.qty
+ticker, side, qty = args.ticker.upper(), args.side.capitalize(), args.qty
 
-session = HTTP(
-    testnet=False,
-    api_key=config.api_key,
-    api_secret=config.api_secret,
-)
+session = HTTP(testnet=False, api_key=config.api_key, api_secret=config.api_secret)
 
 try:
-    response_for_positions = session.get_positions(
-        category="linear",
-        symbol=ticker,
-    )
+    response_for_positions = session.get_positions(category="linear", symbol=ticker)
     positions = response_for_positions['result']['list']
 except Exception as e:
     print(f"Error fetching positions: {e}")
     exit(1)
 
-has_long_position = False
-has_short_position = False
+has_long_position, has_short_position = False, False
 
 for position in positions:
     if position['symbol'] == ticker:
@@ -59,12 +38,6 @@ for position in positions:
             has_short_position = True       
 
 if side == "Buy":
-    if has_short_position:
-        place_order_with_reduce(session, ticker, side, qty)
-    else:
-        place_order(session, ticker, side, qty)
+    place_order_with_reduce(session, ticker, side, qty) if has_short_position else place_order(session, ticker, side, qty)
 elif side == "Sell":
-    if has_long_position:
-        place_order_with_reduce(session, ticker, side, qty)
-    else:
-        place_order(session, ticker, side, qty)
+    place_order_with_reduce(session, ticker, side, qty) if has_long_position else place_order(session, ticker, side, qty)
