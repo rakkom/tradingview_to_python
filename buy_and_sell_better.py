dc7428f170ff3cb25bc74ae3ee53db53b7fa391b
rakkom <127444615+rakkom@users.noreply.github.com>
2023-04-24 00:56:53 +0200

    Update buy_and_sell_better.py

--- buy_and_sell_better.py
+++ buy_and_sell_better.py
@@ -5,7 +5,7 @@ import pybit
 import config
 from pybit.unified_trading import HTTP
 
-def place_order(session, side, ticker, qty, reduce_only=False):
+def place_order(session, ticker, side, qty, reduce_only=False):
     try:
         response = session.place_order(
             category="linear",
@@ -19,23 +19,18 @@ def place_order(session, side, ticker, qty, reduce_only=False):
     except Exception as e:
         print(f"Error placing {side} order: {e}")
 
-def order_with_reduce_only(session, side, ticker, qty):
-    place_order(session, side, ticker, qty, reduce_only=True)
-    place_order(session, side, ticker, qty)
+def place_order_with_reduce(session, ticker, side, qty):
+    place_order(session, ticker, side, qty, reduce_only=True)
+    place_order(session, ticker, side, qty)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--side", help="Trade side (Buy or Sell)", choices=["buy", "sell"])
 parser.add_argument("--ticker", help="Ticker symbol")
+parser.add_argument("--side", help="Trade side (Buy or Sell)")
 parser.add_argument("--qty", help="Quantity")
 args = parser.parse_args()
-
-side = args.side.capitalize()
 ticker = args.ticker.upper()
+side = args.side.capitalize()
 qty = args.qty
-
-if not ticker:
-    print("Ticker symbol is required")
-    exit(1)
 
 session = HTTP(
     testnet=False,
@@ -56,20 +51,24 @@ except Exception as e:
 has_long_position = False
 has_short_position = False
 
-for position in positions:
-    if position['symbol'] == ticker:
-        if position['side'].lower() == 'buy' and float(position['size']) > 0:
-            has_long_position = True
-        elif position['side'].lower() == 'sell' and float(position['size']) > 0:
-            has_short_position = True
+try:
+    for position in positions:
+        if position['symbol'] == ticker:
+            if position['side'].lower() == 'buy' and float(position['size']) > 0:
+                has_long_position = True
+            elif position['side'].lower() == 'sell' and float(position['size']) > 0:
+                has_short_position = True       
+except Exception as e:
+    print("Error caused by fetching position failure")
+    exit(1)
 
 if side == "Buy":
     if has_short_position:
-        order_with_reduce_only(session, side, ticker, qty)
+        place_order_with_reduce(session, ticker, side, qty)
     else:
-        place_order(session, side, ticker, qty)
+        place_order(session, ticker, side, qty)
 elif side == "Sell":
     if has_long_position:
-        order_with_reduce_only(session, ticker, qty)
+        place_order_with_reduce(session, ticker, side, qty)
     else:
-        place_order(session, side, ticker, qty)
+        place_order(session, ticker, side, qty)
